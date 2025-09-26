5c3c31fef9971b54f4568bdf2276b5577408b6d2
John Yang <byjohnyang@gmail.com>
2025-09-26 18:51:38 -0400

    Update robot.js

--- robot.js
+++ robot.js
@@ -2,6 +2,6 @@ function robot(state, unit) {
     if (state.turn % 2 == 0) {
         return Action.move(Direction.East)
     } else {
-	return ACtion.move(Direction.South)
+		return Action.move(Direction.South)
     }
 }
