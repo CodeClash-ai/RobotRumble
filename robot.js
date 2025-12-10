f2aee2c26138550db1f39445723830cf77bf9ffd
John Yang <byjohnyang@gmail.com>
2025-12-10 23:55:59 +0000

    Add bot

--- robot.js
+++ robot.js
@@ -1,7 +1,28 @@
+// strategy: move towards center, attack any adjacent robots encountered along the way
+
+const center = new Coords(MAP_SIZE/2, MAP_SIZE/2)
+
 function robot(state, unit) {
-    if (state.turn % 2 == 0) {
-        return Action.move(Direction.East)
-    } else {
-		return Action.move(Direction.South)
+  let moveDir = unit.coords.directionTo(center)
+  
+  const moveDirs = [Direction.North, Direction.East, Direction.South, Direction.West]
+  const attackDir = moveDirs.reduce((acc, dir) => {
+    if (acc) return acc
+    
+    let neighbor = state.objByCoords(unit.coords.add(dir))
+    if (neighbor) {
+      if (neighbor.objType === ObjType.Unit && neighbor.team !== unit.team) {
+        return dir
+      }
     }
+    
+    return undefined
+  }, undefined)
+  
+  if (attackDir) {
+    return Action.attack(attackDir)
+  }
+  
+  return Action.move(moveDir)
 }
+  
