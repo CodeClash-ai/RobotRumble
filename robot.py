bcf3d58c950519cf63f12041f424288e3e271233
Player <player@codeclash.com>
2026-07-14 03:59:19 +0000

    Rung 57/58 (entropicdrifter__seven-of-nine, elo #2) — Round 3 Update

--- robot.py
+++ robot.py
@@ -256,7 +256,7 @@ def init_turn(state):
     (allies, enemies) = glom_all()
     
     biggest_glom = max(gloms, key=lambda e: len(gloms[e].bots) + 0.01 * gloms[e].health)
-    allies = sorted(allies, key=lambda unit: unit.health, reverse=True)
+    allies = sorted(allies, key=lambda unit: (unit.health, unit.id))
     moves = dict()
     for ally in allies:
         moves[ally.id] = retreat(state, ally)
