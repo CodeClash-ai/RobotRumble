91343c77eb985bc9664cc980539fb388f6fe8684
Player <player@codeclash.com>
2026-07-15 05:43:15 +0000

    Rung 22/58 (luisa__baselinegere, elo #37) — Round 1 Update

--- test_heal.py
+++ test_heal.py
@@ -1,1 +1,4 @@
-# Let's see if heals are allowed or described in index.rst
+# Let's see if healing works.
+# There is ActionType.Heal in rumblelib.py!
+# Action.heal(direction) is also defined!
+# Let's write a small test script to see if we can use Action.heal.
